83434c9b81174ffac60a31b608cf8664999202da
jack5661 <34701860+jack5661@users.noreply.github.com>
2020-05-21 13:34:00 -0700

    Updates

---
+++ precache-manifest.ed35fff5e597ec295a18ff995797a1ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e4367e0cccee4f55943a7233dd2643e",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "4f3c661a298de9ad7783",
+    "url": "/weather-app/static/css/main.ceb7d629.chunk.css"
+  },
+  {
+    "revision": "028c219b9f3d638b98ad",
+    "url": "/weather-app/static/js/2.0e9aaf03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.0e9aaf03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f3c661a298de9ad7783",
+    "url": "/weather-app/static/js/main.e2b19325.chunk.js"
+  },
+  {
+    "revision": "b583aba1370634b5a5a2",
+    "url": "/weather-app/static/js/runtime-main.60cbc992.js"
+  }
+]);
